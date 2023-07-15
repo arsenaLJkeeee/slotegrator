6079960b54ccd6e1014b13e24366e2baf5efe818
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-15 17:07:34 +0300

    add duration

--- src/test/java/pro/slotegrator/SlotegratorTests.java
+++ src/test/java/pro/slotegrator/SlotegratorTests.java
@@ -110,6 +110,7 @@ public class SlotegratorTests extends SlotegratorTestBase{
 
 
             $("#form-name")
+                    .shouldBe(visible, Duration.ofSeconds(10))
                     .setValue("Vladimir Borchevskiy");
             $("#form-email")
                     .setValue("arsenaljkeeee10@gmail.com");
